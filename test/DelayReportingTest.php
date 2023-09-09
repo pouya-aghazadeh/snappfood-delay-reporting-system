de3e69cb408d5069c208e6efd83214f087fc115a
P.Aghazadeh <pouya8287@gmail.com>
2023-09-09 09:08:53 +0330

    [+] Test Structure Added

---
+++ test/DelayReportingTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class DelayReportingTest extends TestCase{
+    public function RegisteringADelayReportForAnOrder()
+    {
+        // test scenario implements here
+    }
+}
